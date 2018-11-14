88345343da3c3507493af67f7ec4f89565478364
gpyshenko <gpyshenko@gmail.com>
2018-11-14 09:11:04 +0500

    Next

--- modules/01-module/07-zip/stream-zip.js
+++ modules/01-module/07-zip/stream-zip.js
@@ -5,3 +5,10 @@ const file = 'test.txt';
 fs
     .createReadStream(file)
     .pipe(zlib.createGzip())
+    .on('end', () => {
+        console.log('Read end');
+    })
+    .pipe(fs.createWriteStream(file + '.gz'))
+    .on('close', () => {
+        console.log('Closed');
+    })
